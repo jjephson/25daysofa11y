e45175b17c4a30562ffec916a20b51fec6e1ca4e
Jimmie Jephson <jimmie.jephson@gmail.com>
2025-10-02 12:04:11 +0200

    Add GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://25daysofa11y.vercel.app',
-  output: 'static'
+  site: 'https://25daysofa11y.com',
+  output: 'static',
+  base: '/',
+  trailingSlash: 'never'
 });
